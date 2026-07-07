965d7ad5534f76c8a6fa1b0498441021b6f45e47
victerou <ogiribovicterou@gmail.com>
2026-07-07 08:21:18 +0100

    Update axios base URL to new endpoint

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://aaf2b248436f04c76be85227a9f32175-1821011344.us-east-1.elb.amazonaws.com:8080',
+    baseURL:'http://35.170.72.69:8080',
     headers: {
         'Content-Type': 'application/json',
     },
